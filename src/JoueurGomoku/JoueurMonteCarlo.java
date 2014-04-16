11c3a40659aefad0fcf2618704677ced5bdc8f58
benoitvuillemin <benoitvuillemin@wifi-etu-0857.univ-lyon1.fr>
2014-04-16 08:59:14 +0200

    Modifs pour MonteCarlo

--- src/JoueurGomoku/JoueurMonteCarlo.java
+++ src/JoueurGomoku/JoueurMonteCarlo.java
@@ -5,34 +5,42 @@
  */
 package JoueurGomoku;
 
+import JeuDeGomoku.*;
 import gomoku.*;
 import java.util.ArrayList;
 
 public class JoueurMonteCarlo extends Joueur {
 
     private int nbSimulation;
-    //private JeuDePlateauFactory factory;
-    public JoueurMonteCarlo(int _id, int _nbSimulation/*, JeuDePlateauFactory _factory*/) {
+    private JeuDePlateauFactory factory;
+
+    public JoueurMonteCarlo(int _id, int _nbSimulation, JeuDePlateauFactory _factory) {
         super(_id);
         this.nbSimulation = _nbSimulation;
-        //this.factory = _factory;
+        this.factory = _factory;
     }
+
     @Override
-    public Coup genererCoup(Plateau etatJeu)
-    {
+    public Coup genererCoup(Plateau etatJeu) {
         Noeud meilleurCoup = null;
         ArrayList<Position> positionsPossibles = etatJeu.etatId(0);
-        for (Position p : positionsPossibles)
-        {
+        int a;
+        for (Position p : positionsPossibles) {
             Coup cCourant = new Coup(id, p);
             Noeud nCourant = new Noeud(cCourant);
             etatJeu.jouer(cCourant);
-            //ArrayList<Coup> sit = etatJeu.getSituation();
-            // Faire les choses de l'énoncé
-            /*if((meilleurCoup == null)||(meilleurCoup < nCourant))
-            {
+            ArrayList<Coup> sit = etatJeu.getSituation();
+            for (int i = 0; i < this.nbSimulation; i++) {
+                a = this.factory.CreerPartieAleatoireVSAleatoire(sit).jouerPartie().getId();
+                if (a == 2) {
+                    nCourant.ajouterVictoire();
+                } else {
+                    nCourant.ajouterDefaite();
+                }
+            }
+            if ((meilleurCoup == null) || (meilleurCoup.getMoyenne() < nCourant.getMoyenne())) {
                 meilleurCoup = nCourant;
-            }*/
+            }
             etatJeu.annuler();
         }
         return meilleurCoup.getCoup();
